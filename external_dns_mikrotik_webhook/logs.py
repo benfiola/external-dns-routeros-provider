ef62934561ed5e211100c8b03813a937c7cfc52d
Ben Fiola <benfiola@gmail.com>
2024-03-23 17:50:29 -0700

    Add logging comment

--- external_dns_mikrotik_webhook/logs.py
+++ external_dns_mikrotik_webhook/logs.py
@@ -7,6 +7,10 @@ import external_dns_mikrotik_webhook
 
 
 def configure_logging(log_level: int | None = None):
+    """
+    Configures logging for the application.
+    """
+    # configure the logger for the application
     log_level = log_level or logging.INFO
     logger = logging.getLogger(external_dns_mikrotik_webhook.__name__)
     handler = logging.StreamHandler()
@@ -17,6 +21,9 @@ def configure_logging(log_level: int | None = None):
     logger.addHandler(handler)
     logger.setLevel(log_level)
 
+    # configure uvicorn logging
+    # NOTE: reference: uvicorn.config.LOGGING_CONFIG
+    # NOTE: reference: uvicorn.logging.AccessFormatter
     logger = logging.getLogger("uvicorn.access")
     handler = logging.StreamHandler()
     access_formatter = uvicorn.logging.AccessFormatter(
@@ -26,7 +33,6 @@ def configure_logging(log_level: int | None = None):
     logger.addHandler(handler)
     logger.propagate = False
     logger.setLevel(log_level)
-
     logger = logging.getLogger("uvicorn")
     handler = logging.StreamHandler()
     handler.setFormatter(default_formatter)
